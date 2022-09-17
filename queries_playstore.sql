09634b16e82a891262a15f9d8a29b683850d42d0
Jimmy Austin <jimmycaustin@gmail.com>
2022-09-17 06:59:07 -0400

    initial commit

---
+++ queries_playstore.sql
@@ -0,0 +1,31 @@
+-- Comments in SQL Start with dash-dash --
+
+1) SELECT * FROM analytics WHERE id = 1880;
+
+2) SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+
+3) SELECT category, COUNT(category) FROM analytics GROUP BY category;
+
+4)  SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
+
+5) SELECT app_name FROM analytics WHERE reviews > 4.8 ORDER BY reviews DESC LIMIT 1;
+
+6) SELECT category, avg(rating) AS avg_rating FROM analytics GROUP BY category ORDER BY avg(rating) DESC;
+
+7) SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+
+8) SELECT app_name FROM analytics WHERE min_installs <= 50 AND rating BETWEEN 1 AND 5 ORDER BY rating DESC;
+
+9) SELECT app_name FROM analytics WHERE rating < 3 AND reviews >= 10000;
+
+10) SELECT app_name FROM analytics WHERE price BETWEEN 0.10 AND 1.00 ORDER BY reviews DESC LIMIT 10;
+
+11) SELECT app_name FROM analytics ORDER BY last_updated ASC LIMIT 1;
+
+12) SELECT app_name FROM analytics ORDER BY price DESC LIMIT 1;
+
+13) SELECT COUNT(reviews) FROM analytics;
+
+14) SELECT category FROM analytics GROUP BY category HAVING COUNT(category) > 300;
+
+15) SELECT app_name, reviews, min_installs, (min_installs/reviews) FROM analytics WHERE min_installs > 100000 ORDER BY (min_installs/reviews) DESC LIMIT 1;
